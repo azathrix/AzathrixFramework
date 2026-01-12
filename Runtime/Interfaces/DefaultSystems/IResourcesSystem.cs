f119c557db4cae2b7b520c63293fee944489c019
InputName <xiahogzhi@qq.com>
2026-01-13 00:09:27 +0800

    屏蔽resource系统

--- Runtime/Interfaces/DefaultSystems/IResourcesSystem.cs
+++ Runtime/Interfaces/DefaultSystems/IResourcesSystem.cs
@@ -1,28 +1,28 @@
-using Cysharp.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-namespace Azathrix.Framework.Interfaces.DefaultSystems
-{
-    /// <summary>
-    /// 资源系统接口，扩展基础加载功能
-    /// </summary>
-    public interface IResourcesSystem : IResourcesLoader, ISystem
-    {
-        /// <summary>
-        /// 实例化GameObject
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="parent"></param>
-        /// <returns></returns>
-        UniTask<GameObject> InstantiateAsync(string key, Transform parent = null);
-        
-        /// <summary>
-        /// 加载场景
-        /// </summary>
-        /// <param name="key"></param>
-        /// <param name="mode"></param>
-        /// <returns></returns>
-        UniTask LoadSceneAsync(string key, LoadSceneMode mode);
-    }
-}
+// using Cysharp.Threading.Tasks;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+//
+// namespace Azathrix.Framework.Interfaces.DefaultSystems
+// {
+//     /// <summary>
+//     /// 资源系统接口，扩展基础加载功能
+//     /// </summary>
+//     public interface IResourcesSystem : IResourcesLoader, ISystem
+//     {
+//         /// <summary>
+//         /// 实例化GameObject
+//         /// </summary>
+//         /// <param name="key"></param>
+//         /// <param name="parent"></param>
+//         /// <returns></returns>
+//         UniTask<GameObject> InstantiateAsync(string key, Transform parent = null);
+//         
+//         /// <summary>
+//         /// 加载场景
+//         /// </summary>
+//         /// <param name="key"></param>
+//         /// <param name="mode"></param>
+//         /// <returns></returns>
+//         UniTask LoadSceneAsync(string key, LoadSceneMode mode);
+//     }
+// }
