e32e187aa6aca175e5e591c5928bb89b6a9848d5
InputName <xiahogzhi@qq.com>
2026-01-15 03:00:26 +0800

    移除多余内置启动阶段

--- Runtime/Core/Startup/Interfaces/Phases/IAssemblyLoadPhase.cs
+++
@@ -1,7 +0,0 @@
-namespace Azathrix.Framework.Core.Startup.Phases
-{
-    /// <summary>
-    /// 程序集加载阶段 (Order: 100)
-    /// </summary>
-    public interface IAssemblyLoadPhase : IStartupPhase { }
-}
--- Runtime/Core/Startup/Interfaces/Phases/IResourceLoadPhase.cs
+++
@@ -1,7 +0,0 @@
-namespace Azathrix.Framework.Core.Startup.Phases
-{
-    /// <summary>
-    /// 资源加载阶段 (Order: 0)
-    /// </summary>
-    public interface IResourceLoadPhase : IStartupPhase { }
-}
